4d3c2338b6194428edcacd5b648961716e07889d
Julien_macbook <nttmxk@korea.ac.kr>
2022-08-04 18:14:49 +0900

    specular applied

--- tutorial.c
+++ tutorial.c
@@ -11,10 +11,10 @@ int main(int ac, char **av)
 //	cc -lmlx -framework OpenGL -framework AppKit *.c
 	t_data data;
 
-	if (ac != 2)
-		return 1; // error
-	parse_mini_rt(av[1]);
-	return 0;
+//	if (ac != 2)
+//		return 1; // error
+//	parse_mini_rt(av[1]);
+//	return 0;
 	data_init(&data);
 	mlx_key_hook(data.win, press_key, &data);
 	mlx_loop_hook(data.mlx, loop_func, &data);
